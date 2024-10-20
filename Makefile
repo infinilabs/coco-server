84cdc6838c2d7eb7bdfbdc35517fa7d285adab35
medcl <m@medcl.net>
2024-10-20 23:41:15 +0800

    chore: init project structure

---
+++ Makefile
@@ -0,0 +1,13 @@
+SHELL=/bin/bash
+
+# APP info
+APP_NAME := coco
+APP_VERSION := 1.0.0_SNAPSHOT
+APP_CONFIG := $(APP_NAME).yml
+APP_EOLDate ?= "2025-12-31T10:10:10Z"
+APP_STATIC_FOLDER := .public
+APP_STATIC_PACKAGE := public
+APP_UI_FOLDER := ui
+APP_PLUGIN_FOLDER := plugins
+
+include ../framework/Makefile
---
+++ main.go
@@ -0,0 +1,41 @@
+/* Copyright © INFINI Ltd. All rights reserved.
+ * web: https://infinilabs.com
+ * mail: hello#infini.ltd */
+
+package main
+
+import (
+	"infini.sh/coco/config"
+	"infini.sh/framework"
+	"infini.sh/framework/core/module"
+	"infini.sh/framework/core/util"
+	stats "infini.sh/framework/plugins/stats_statsd"
+)
+
+func main() {
+
+	terminalHeader := ("   ___  ___  ___  ___     _     _____ \n")
+	terminalHeader += ("  / __\\/___\\/ __\\/___\\   /_\\    \\_   \\\n")
+	terminalHeader += (" / /  //  // /  //  //  //_\\\\    / /\\/\n")
+	terminalHeader += ("/ /__/ \\_// /__/ \\_//  /  _  \\/\\/ /_  \n")
+	terminalHeader += ("\\____|___/\\____|___/   \\_/ \\_/\\____/  \n")
+
+
+	terminalFooter := ("")
+
+	app := framework.NewApp("coco", "Search, connect, collaborate – all in one place.",
+		util.TrimSpaces(config.Version), util.TrimSpaces(config.BuildNumber), util.TrimSpaces(config.LastCommitLog), util.TrimSpaces(config.BuildDate), util.TrimSpaces(config.EOLDate), terminalHeader, terminalFooter)
+
+	app.IgnoreMainConfigMissing()
+	app.Init(nil)
+
+	defer app.Shutdown()
+
+	if app.Setup(func() {
+		module.RegisterUserPlugin(&stats.StatsDModule{})
+		module.Start()
+	}, func() {
+	}, nil) {
+		app.Run()
+	}
+}
