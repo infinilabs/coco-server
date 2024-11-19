63ed81e3272bb55f9a36f896b6ed7489a71f267e
medcl <m@medcl.net>
2024-11-19 18:44:17 +0800

    feat: incremental indexing google drive files, connect via url

---
+++ plugins/connectors/google_drive/api.go
@@ -0,0 +1,148 @@
+/* Copyright © INFINI LTD. All rights reserved.
+ * Web: https://infinilabs.com
+ * Email: hello#infini.ltd */
+
+package google_drive
+
+import (
+	"encoding/base64"
+	"fmt"
+	"golang.org/x/oauth2"
+	httprouter "infini.sh/framework/core/api/router"
+	"infini.sh/framework/core/errors"
+	"infini.sh/framework/core/kv"
+	"infini.sh/framework/core/util"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func encodeState(args map[string]string) string {
+	var stateParts []string
+	for key, value := range args {
+		stateParts = append(stateParts, fmt.Sprintf("%s=%s", key, url.QueryEscape(value)))
+	}
+	return base64.URLEncoding.EncodeToString([]byte(strings.Join(stateParts, "&")))
+}
+
+func decodeState(state string) (map[string]string, error) {
+	decoded, err := base64.URLEncoding.DecodeString(state)
+	if err != nil {
+		return nil, err
+	}
+
+	args := map[string]string{}
+	for _, part := range strings.Split(string(decoded), "&") {
+		kv := strings.SplitN(part, "=", 2)
+		if len(kv) == 2 {
+			args[kv[0]], _ = url.QueryUnescape(kv[1])
+		}
+	}
+	return args, nil
+}
+
+// Endpoint to initiate the OAuth flow
+func (h *Plugin) connect(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+
+	//TODO handle tenant info
+	// Custom arguments to pass
+	customArgs := map[string]string{
+		"tenant":   "test",
+		"user":     "test",
+		"redirect": "/connector/connect_success",
+	}
+
+	// Encode customArgs into a single string
+	state := encodeState(customArgs)
+
+	// Generate OAuth URL
+	authURL := h.oAuthConfig.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	http.Redirect(w, req, authURL, http.StatusFound)
+}
+
+// Endpoint to handle the OAuth redirect
+func (h *Plugin) oAuthRedirect(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+
+	// Retrieve the state parameter from the query
+	state := req.URL.Query().Get("state")
+	if state == "" {
+		panic(errors.New("Missing 'state' parameter"))
+	}
+
+	// Decode the state parameter
+	customArgs, err := decodeState(state)
+	if err != nil {
+		panic(err)
+	}
+
+	//// Access custom arguments
+	tenantID := customArgs["tenant"]
+	userID := customArgs["user"]
+	redirectPath := customArgs["redirect"]
+
+	// Extract the code from the query parameters
+	code := req.URL.Query().Get("code")
+	if code == "" {
+		panic(err)
+	}
+
+	// Exchange the authorization code for an access token
+	token, err := h.oAuthConfig.Exchange(req.Context(), code)
+	if err != nil {
+		panic(err)
+	}
+
+	err=h.saveToken(tenantID,userID,token)
+	if err != nil {
+		panic(err)
+	}
+
+	newRedirectUrl:=util.JoinPath(redirectPath,"?source=google_drive")
+	h.Redirect(w,req,newRedirectUrl)
+}
+
+
+
+func (this *Plugin) saveLastModifiedTime(tenantID,userID string,lastModifiedTime string) error  {
+	key := strings.Join([]string{tenantID, userID}, ",")
+	err:= kv.AddValue("/connector/google_drive/lastModifiedTime", []byte(key), []byte(lastModifiedTime))
+	return err
+}
+
+func (this *Plugin) getLastModifiedTime(tenantID,userID string) (string,error) {
+	key := strings.Join([]string{tenantID, userID}, ",")
+
+	data,err := kv.GetValue("/connector/google_drive/lastModifiedTime", []byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func (this *Plugin) saveToken(tenantID,userID string,token *oauth2.Token) error  {
+	key := strings.Join([]string{tenantID, userID}, ",")
+	err:= kv.AddValue("/connector/google_drive/token", []byte(key), util.MustToJSONBytes(token))
+	return err
+}
+
+func (this *Plugin) getToken(tenantID,userID string) (bool,*oauth2.Token,error) {
+	key := strings.Join([]string{tenantID, userID}, ",")
+
+	data,err := kv.GetValue("/connector/google_drive/token", []byte(key))
+	if err != nil {
+		return false,nil, err
+	}
+
+	if data!=nil{
+		obj:=oauth2.Token{}
+		err=util.FromJSONBytes(data,&obj)
+		if err!=nil{
+			return false,nil, err
+		}
+
+		return true,&obj, nil
+	}
+
+	return false,nil, nil
+}
--- plugins/connectors/google_drive/plugin.go
+++ plugins/connectors/google_drive/plugin.go
@@ -5,17 +5,44 @@
 package google_drive
 
 import (
+	"context"
+	log "github.com/cihub/seelog"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"infini.sh/framework/core/api"
 	"infini.sh/framework/core/env"
 	"infini.sh/framework/core/global"
 	"infini.sh/framework/core/module"
 	"infini.sh/framework/core/queue"
+	"infini.sh/framework/core/task"
+	"infini.sh/framework/core/util"
+	"os"
+	"time"
 )
 
 type Plugin struct {
-	Enabled     bool   `config:"enabled"`
-	Credentials string `config:"credentials"`
-	Token       string `config:"token"`
-	Queue  *queue.QueueConfig `config:"queue"`
+	api.Handler
+
+	Enabled          bool               `config:"enabled"`
+	Interval         string             `config:"interval"`
+	PageSize         int64                `config:"page_size"`
+	CredentialFile   string             `config:"credential_file"`
+	Credential       *Credential        `config:"credential"`
+	SkipInvalidToken bool               `config:"skip_invalid_token"`
+	Queue            *queue.QueueConfig `config:"queue"`
+	oAuthConfig      *oauth2.Config
+}
+
+type Credential struct {
+	ClientId                string   `config:"client_id" json:"client_id"`
+	ProjectId               string   `config:"project_id" json:"project_id"`
+	AuthUri                 string   `config:"auth_uri" json:"auth_uri"`
+	TokenUri                string   `config:"token_uri" json:"token_uri"`
+	AuthProviderX509CertUrl string   `config:"auth_provider_x509_cert_url" json:"auth_provider_x509_cert_url"`
+	ClientSecret            string   `config:"client_secret" json:"client_secret"`
+	RedirectUri             string   `config:"redirect_uris" json:"redirect_uris"`
+	JavascriptOrigins       []string `config:"javascript_origins" json:"javascript_origins"`
 }
 
 func (this *Plugin) Setup() {
@@ -23,16 +50,83 @@ func (this *Plugin) Setup() {
 	if ok && err != nil && global.Env().SystemConfig.Configs.PanicOnConfigError {
 		panic(err)
 	}
-	if this.Queue==nil{
-		this.Queue=&queue.QueueConfig{Name: "indexing_documents"}
+	if this.Queue == nil {
+		this.Queue = &queue.QueueConfig{Name: "indexing_documents"}
 	}
+
+	if this.CredentialFile != "" {
+		b, err := os.ReadFile(this.CredentialFile)
+		if err != nil {
+			panic(err)
+		}
+
+		// Load credentials
+		this.oAuthConfig, err = google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+		if err != nil {
+			panic(err)
+		}
+	} else if this.Credential != nil {
+
+		if this.Credential.ClientId == "" || this.Credential.ClientSecret == "" || len(this.Credential.RedirectUri) == 0 {
+			panic("Missing Google OAuth credentials")
+		}
+
+		this.oAuthConfig = &oauth2.Config{
+			ClientID:     this.Credential.ClientId,
+			ClientSecret: this.Credential.ClientSecret,
+			RedirectURL:  this.Credential.RedirectUri,
+			Scopes:       []string{"https://www.googleapis.com/auth/drive.metadata.readonly"},
+			Endpoint:     google.Endpoint,
+		}
+	}
+
+	api.HandleAPIMethod(api.GET, "/connector/google_drive/connect", this.connect)
+	api.HandleAPIMethod(api.GET, "/connector/google_drive/oauth_redirect", this.oAuthRedirect)
 
 }
 
 func (this *Plugin) Start() error {
 
 	if this.Enabled {
-		startIndexingFiles(this.Credentials, this.Token,this.Queue)
+		//get all accounts which enabled google drive connector
+
+		task.RegisterScheduleTask(task.ScheduleTask{
+			ID:          util.GetUUID(),
+			Group:       "connectors",
+			Singleton:   true,
+			Interval:    util.GetDurationOrDefault(this.Interval, time.Second*30).String(),
+			Description: "indexing google drive files",
+			Task: func(ctx context.Context) {
+
+				log.Tracef("entering task, indexing google drive files")
+
+				//TODO
+				var tenantID = "test"
+				var userID = "test"
+
+				exists,tok, err := this.getToken(tenantID, userID)
+				if err != nil {
+					panic(err)
+				}
+
+				if !exists{
+					return
+				}
+
+				if !tok.Valid() {
+					//continue //TODO
+					if !this.SkipInvalidToken && !tok.Valid() {
+						panic("token is invalid")
+					}
+					log.Warnf("skip invalid token: %v",tok)
+				} else {
+					log.Debug("start processing google drive files")
+					this.startIndexingFiles(tenantID,userID,tok)
+					log.Debug("finished process google drive files")
+				}
+			},
+		})
+
 	}
 
 	return nil
@@ -47,5 +141,5 @@ func (this *Plugin) Name() string {
 }
 
 func init() {
-	module.RegisterUserPlugin(&Plugin{})
+	module.RegisterUserPlugin(&Plugin{SkipInvalidToken: true,PageSize: 10})
 }
