6197af89dc4eadaf9cf6fd710ce43e44d29c817c
Bin Wu <binwu@google.com>
2024-11-20 06:33:44 +0000

    mount logs and data volume for ES docker and auto update coco.yml password

--- Install.sh
+++ Install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+pwd=`pwd`
+
 __ollama() {
     which ollama || curl -fsSL https://ollama.com/install.sh | sh
     OLLAMA_HOST=0.0.0.0:11434 ollama serve
@@ -15,13 +17,25 @@ __ocr_server() {
 }
 
 __easyserch() {
+    [ -d $pwd/easysearch ] || mkdir -p $pwd/easysearch/{data,logs}
+    sudo chown -R 602:602 $pwd/easysearch
+
     docker run \
         -itd --name easysearch \
+        --hostname easysearch \
+        --restart=unless-stopped \
         -p 9200:9200 \
+        -v $pwd/easysearch/data:/app/easysearch/data:rw \
+        -v $pwd/easysearch/logs:/app/easysearch/logs:rw \
         infinilabs/easysearch:1.8.3-265
 
-    pw=$(docker logs easysearch | grep "admin:" | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
-    echo Easysearch admin password: $pw
+    espw=$(docker logs easysearch | grep "admin:" | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
+    echo Easysearch admin password: $espw
+
+    [ -f coco.yml ] && cp coco.yml coco.yml.bak
+
+    egrep -lZ "\%ES_PASSWD\%" ${pwd}/coco.yml \
+      | xargs -0 -l sed -i -e "s/\%ES_PASSWD\%/${espw}/g"
 }
 
 __main() {
