a6fbea849a248eb74e0e43f6f0a9d75eb4051eb1
medcl <m@medcl.net>
2025-02-15 15:58:36 +0800

    refactor: refactoring search api

--- modules/search/search.go
+++ modules/search/search.go
@@ -85,6 +85,57 @@ func (h APIHandler) search(w http.ResponseWriter, req *http.Request, ps httprout
 		source       = h.GetParameterOrDefault(req, "source_fields", "*")
 	)
 
+	mustClauses := BuildMustClauses(datasource, category, subcategory, richCategory, username, userid)
+
+	var q *orm.Query
+	if query != "" || len(mustClauses) > 0 {
+		q = BuildTemplatedQuery(from, size, mustClauses, field, query, source, tags)
+	} else {
+		body, err := h.GetRawBody(req)
+		if err != nil {
+			http.Error(w, "query must be provided", http.StatusBadRequest)
+			return
+		}
+		if len(body) == 0 {
+			//ignore empty query
+			return
+		}
+		q = &orm.Query{}
+		q.RawQuery = body
+	}
+
+	docs := []common.Document{}
+	err, res := orm.SearchWithJSONMapper(&docs, q)
+	if err != nil {
+		h.WriteError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = h.Write(w, res.Raw)
+	if err != nil {
+		h.Error(w, err)
+	}
+}
+
+func BuildTemplatedQuery(from int, size int, mustClauses []interface{}, field string, query string, source string, tags string) *orm.Query {
+	templatedQuery := orm.TemplatedQuery{}
+	templatedQuery.TemplateID = "coco-query-string"
+
+	templatedQuery.Parameters = util.MapStr{
+		"from":         from,
+		"size":         size,
+		"must_clauses": mustClauses,
+		"field":        field,
+		"query":        query,
+		"source":       strings.Split(source, ","),
+		"tags":         strings.Split(tags, ","),
+	}
+	q := orm.Query{}
+	q.TemplatedQuery = &templatedQuery
+	return &q
+}
+
+func BuildMustClauses(datasource string, category string, subcategory string, richCategory string, username string, userid string) []interface{} {
 	mustClauses := []interface{}{}
 
 	// Check and add conditions to mustClauses
@@ -135,44 +186,5 @@ func (h APIHandler) search(w http.ResponseWriter, req *http.Request, ps httprout
 			},
 		})
 	}
-
-	q := orm.Query{}
-	if query != "" || len(mustClauses) > 0 {
-		templatedQuery := orm.TemplatedQuery{}
-		templatedQuery.TemplateID = "coco-query-string"
-
-		templatedQuery.Parameters = util.MapStr{
-			"from":         from,
-			"size":         size,
-			"must_clauses": mustClauses,
-			"field":        field,
-			"query":        query,
-			"source":       strings.Split(source, ","),
-			"tags":         strings.Split(tags, ","),
-		}
-		q.TemplatedQuery = &templatedQuery
-	} else {
-		body, err := h.GetRawBody(req)
-		if err != nil {
-			http.Error(w, "query must be provided", http.StatusBadRequest)
-			return
-		}
-		if len(body) == 0 {
-			//ignore empty query
-			return
-		}
-		q.RawQuery = body
-	}
-
-	docs := []common.Document{}
-	err, res := orm.SearchWithJSONMapper(&docs, &q)
-	if err != nil {
-		h.WriteError(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	_, err = h.Write(w, res.Raw)
-	if err != nil {
-		h.Error(w, err)
-	}
+	return mustClauses
 }
