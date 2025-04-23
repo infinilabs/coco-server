3a1c5ee8897b1f5325d4af53b68e6da4c10d1177
liugq <silenceqi@hotmail.com>
2025-04-23 15:49:02 +0800

    chore: add ellipsis to connector column description

--- web/src/pages/settings/modules/Connector.tsx
+++ web/src/pages/settings/modules/Connector.tsx
@@ -207,7 +207,8 @@ const ConnectorSettings = memo(() => {
     {
       dataIndex: 'description',
       minWidth: 100,
-      title: t('page.connector.columns.description')
+      title: t('page.connector.columns.description'),
+      ellipsis: true,
     },
     {
       dataIndex: 'tags',
