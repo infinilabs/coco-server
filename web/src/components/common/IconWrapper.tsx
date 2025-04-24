967e45426c3fe7ac7a1271cde33b7344ef4ff2b8
yaojiping <yaojiping@infini.ltd>
2025-04-24 11:01:29 +0800

    chore: adjust icon's styles in dark mode

--- web/src/components/common/IconWrapper.tsx
+++ web/src/components/common/IconWrapper.tsx
@@ -1,9 +1,12 @@
-const Icon = (props) => {
+import { getDarkMode } from "@/store/slice/theme";
 
+const Icon = (props) => {
     const { className = '', children } = props
+    
+    const darkMode = useAppSelector(getDarkMode);
 
     return (
-        <div className={`overflow-hidden bg-#fff border border-[var(--ant-color-border)] rounded-[50%] flex items-center justify-center ${className}`}>
+        <div className={`overflow-hidden flex items-center justify-center ${className}`} style={darkMode ? { filter: `drop-shadow(0 0 6px rgba(255, 255, 255, 1))`} : {}}>
             {children}
         </div>
     )
