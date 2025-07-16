acb86b8dae9823fdf87be22920335307c36fcd60
yaojp123 <15989103230@163.com>
2025-07-16 11:25:01 +0800

    fix: add local icons (#362)
    Co-authored-by: yaojiping <yaojiping@infini.ltd>

--- web/src/icons/index.js
+++ web/src/icons/index.js
@@ -2,6 +2,12 @@
 import { addCollection } from '@iconify/react';
 import mdiIcons from '@iconify/json/json/mdi.json'; 
 import phIcons from '@iconify/json/json/ph.json'; 
+import heroiconsIcons from '@iconify/json/json/heroicons.json';
+import materialSymbolsIcons from '@iconify/json/json/material-symbols.json'; 
+import lineMdIcons from '@iconify/json/json/line-md.json'; 
 
 addCollection(mdiIcons);
 addCollection(phIcons);
+addCollection(heroiconsIcons);
+addCollection(materialSymbolsIcons);
+addCollection(lineMdIcons);
