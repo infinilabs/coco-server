8b6b111943c0aafd0c73c0b12ab589f7b5094e6d
yaojiping <yaojiping@infini.ltd>
2025-07-17 17:32:15 +0800

    chore: add widget fullscreen

---
+++ web/widgets/fullscreen/src/Fullscreen.jsx
@@ -0,0 +1,171 @@
+import { useEffect } from 'react';
+import { Fullscreen } from './ui-search';
+import './ui-search/index.css';
+
+export default (props) => {
+    const { id, token, server } = props;
+    const [settings, setSettings] = useState()
+
+    const { payload = {} } = settings || {}
+
+    async function fetchSettings(server, id, token) {
+        if (!server || !id || !token) return;
+        fetch(`${server}/integration/${id}`, {
+            headers: {
+                'APP-INTEGRATION-ID': id,
+                'Content-Type': 'application/json',
+                'X-API-TOKEN': token
+            },
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(result => {
+            if (result?._source) {
+                setSettings(result?._source);
+            }
+        })
+        .catch(error => console.log('error', error));
+    }
+    
+    function search(query, callback, setLoading, shouldAgg) {
+        if (setLoading) setLoading(true)
+        const { filters = {} } = query
+        const filterStr = Object.keys(filters).map((key) => `filter=${key}:any(${filters[key].join(',')})`).join('&')
+        fetch(`${server}/query/_search?${filterStr ? filterStr + '&' : ''}query=${query.keyword}&from=${query.from}&size=${query.size}&v2=true`, {
+            method: 'POST',
+            headers: {
+                'app-integration-id': id,
+                'X-API-TOKEN': token
+            },
+            body: shouldAgg ? JSON.stringify({
+                "aggs": {
+                    "category": { "terms": { "field": "category" } },
+                    "lang": { "terms": { "field": "lang" } },
+                    "source.id": { 
+                    "terms":  { 
+                        "field": "source.id" 
+                    },
+                    "aggs": {
+                        "top": {
+                        "top_hits": {
+                            "size": 1,
+                            "_source": ["source.name"]
+                        }
+                        }
+                    }
+                    },   
+                    "type": { "terms": { "field": "type" } } 
+                }
+            }) : undefined
+        })
+        .then(response => {
+            if (!response.ok) throw new Error('response was not ok');
+            return response.json();
+        })
+        .then(data => {
+            callback(data)
+        })
+        .catch(error => {
+            console.error('error:', error);
+            callback({ error })
+        }).finally(() => {
+            if (setLoading) setLoading(false)
+        })
+    }
+
+    async function ask(assistantID, query, callback, setLoading) {
+        setLoading(true)
+        try {
+            const response = await fetch(`${server}/assistant/${assistantID}/_ask`, {
+                headers: {
+                    'app-integration-id': id,
+                    'X-API-TOKEN': token
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    message: query.keyword
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder('utf-8');
+            let lineBuffer = '';
+
+            while (true) {
+            const { done, value } = await reader.read();
+
+            if (done) {
+                setLoading(false)
+                break;
+            }
+
+            const chunk = decoder.decode(value, { stream: true }); 
+
+            lineBuffer += chunk;
+        
+            const lines = lineBuffer.split('\n');
+            for (let i = 0; i < lines.length - 1; i++) {
+                try {
+                    const json = JSON.parse(lines[i]);
+                    if (json && !(json._id && json._source && json.result)) {
+                        callback(json)
+                    }
+                } catch (error) {
+                    console.log("error:", lines[i])
+                }
+            }
+            
+            lineBuffer = lines[lines.length - 1];
+            }
+        } catch (error) {
+            setLoading(false)
+            console.error('error:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchSettings(server, id, token);
+    }, [server, id, token]);
+
+    if (settings?.type !== 'fullscreen') return null;
+
+    return (
+        <Fullscreen {...{
+            "logo": {
+                "light": payload?.logo?.light,
+                "light-mobile": payload?.logo?.['light-mobile'],
+            },
+            "welcome": payload?.welcome || "Nice to meet you. I can help answer your questions by tapping into the internet and your data sources. How can I assist you today?",
+            "aiOverview": payload?.ai_overview?.enabled ? {
+                "assistantID": payload?.ai_overview?.assistant,
+                "title": "AI Overview",
+            } : undefined,
+            "widgets": payload.ai_widgets?.enabled && payload.ai_widgets?.widgets ? payload.ai_widgets?.widgets.map((item) => ({
+                "assistantID": item.assistant,
+                "title": "Suggestions",
+                "showActions": false,
+                "height": item.height ? `${item.height}px` : "auto",
+            })) : [],
+            "onSearch": (query, callback, setLoading, shouldAgg = true) => {
+                search(query, callback, setLoading, shouldAgg)
+            },
+            "onAsk": (assistanID, query, callback, setLoading) => {
+                ask(assistanID, query, callback, setLoading)
+            },
+            "config": {
+                "aggregations": {
+                    "source.id": {
+                        "displayName": "source"
+                    },
+                    "lang": {
+                        "displayName": "language"
+                    }
+                }
+            }
+        }} />
+    )
+}
---
+++ web/widgets/fullscreen/tsup.config.ts
@@ -0,0 +1,25 @@
+import type { Options } from 'tsup';
+import { defineConfig } from 'tsup';
+import svgr from 'esbuild-plugin-svgr';
+
+export default defineConfig(
+  config =>
+    [
+      {
+        clean: true,
+        entry: ['src/index.jsx'],
+        format: ['esm'],
+        minify: true,
+        noExternal: ['react', 'react-dom'],
+        platform: 'browser',
+        sourcemap: config.watch,
+        splitting: false,
+        esbuildPlugins: [svgr()],
+      },
+      {
+        clean: true,
+        entry: ['src/styles/index.css'],
+        minify: true
+      }
+    ] as Options[]
+);
