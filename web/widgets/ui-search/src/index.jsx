60c8e31fa8f8fd77189cb116e13c9f398193ce2c
BiggerRain <15911122312@163.COM>
2025-09-27 09:15:44 +0800

    style: fullscreen ui styles (#531)
    * style: ui styles
    
    * chore: add icon

--- web/widgets/ui-search/src/index.jsx
+++ web/widgets/ui-search/src/index.jsx
@@ -1,29 +1,31 @@
-import { StyleProvider } from '@ant-design/cssinjs';
-import React from 'react';
-import './index.css';
-import { default as Page } from './FullscreenPage'
-import { default as Modal } from './FullscreenModal'
+import { StyleProvider } from "@ant-design/cssinjs";
+import React from "react";
+
+import { default as Page } from "./FullscreenPage";
+import { default as Modal } from "./FullscreenModal";
+
+import "./index.css";
 
 export const FullscreenPage = (props) => {
-    const { shadow } = props;
+  const { shadow } = props;
 
-    return (
-        <React.StrictMode>
-            <StyleProvider container={shadow}>
-                <Page {...props} root={shadow || document}/>
-            </StyleProvider>
-        </React.StrictMode>
-    );
-}
+  return (
+    <React.StrictMode>
+      <StyleProvider container={shadow}>
+        <Page {...props} root={shadow || document} />
+      </StyleProvider>
+    </React.StrictMode>
+  );
+};
 
 export const FullscreenModal = (props) => {
-    const { shadow } = props;
+  const { shadow } = props;
 
-    return (
-        <React.StrictMode>
-            <StyleProvider container={shadow}>
-                <Modal {...props} root={shadow || document}/>
-            </StyleProvider>
-        </React.StrictMode>
-    );
-}
+  return (
+    <React.StrictMode>
+      <StyleProvider container={shadow}>
+        <Modal {...props} root={shadow || document} />
+      </StyleProvider>
+    </React.StrictMode>
+  );
+};
