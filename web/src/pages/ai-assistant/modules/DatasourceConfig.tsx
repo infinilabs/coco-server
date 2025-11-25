3dddaf36fa040704d3df50948b09a9bf08d01ba2
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-11-25 11:20:05 +0800

    refactor: refactor the assistant ui styles (#589)
    * refactor: refactor the assistant ui styles
    
    * refactor: update i18n

--- web/src/pages/ai-assistant/modules/DatasourceConfig.tsx
+++ web/src/pages/ai-assistant/modules/DatasourceConfig.tsx
@@ -1,58 +1,21 @@
-import { Select, Space, Switch, Input } from "antd";
+import { Flex, Form, Input, Select, Space, Switch } from 'antd';
 
 interface DatasourceConfigProps {
-  value?: any;
-  onChange?: (value: string) => void;
-  options: any[];
-  loading: boolean
+  readonly value?: any;
+  readonly onChange?: (value: string) => void;
+  readonly options: any[];
+  readonly loading: boolean;
 }
 
 export const DatasourceConfig = (props: DatasourceConfigProps) => {
   const { t } = useTranslation();
   const { loading, value = {}, onChange } = props;
-  const onIDsChange = (newIds: string[]) => {
-    if (onChange) {
-      onChange({
-        ...value,
-        ids: newIds,
-      });
-    }
-  };
-  const onEnabledChange = (enabled: boolean) => {
-    onChange?.({
-      ...value,
-      enabled
-    });
-  };
-  const onVisibleChange = (visible: boolean) => {
-    onChange?.({
-      ...value,
-      visible,
-    });
-  };
-  const onEnabled_by_defaultChange = (enabled_by_default: boolean) => {
-    onChange?.({
-      ...value,
-      enabled_by_default,
-    });
-  };
 
-  const [showFilter, setShowFilter] = useState(!!value.filter);
+  const [showFilter, setShowFilter] = useState(Boolean(value.filter));
+
   const onFilterToggle = () => {
     setShowFilter(!showFilter);
   };
-
-  const onFilterChange = (evt: any) => {
-    const filter = evt.target.value || null;
-    onChange?.({
-      ...value,
-      filter,
-    });
-  };
-
-  if (isObject(value.filter)) {
-    value.filter = JSON.stringify(value.filter);
-  }
 
   const filterPlaceHolder = `{
   "term": {
@@ -60,65 +23,80 @@ export const DatasourceConfig = (props: DatasourceConfigProps) => {
   }
 }`;
 
+  useEffect(() => {
+    if (value.visible) return;
+
+    onChange?.({
+      ...value,
+      enabled_by_default: true
+    });
+  }, [value.visible]);
+
   return (
-    <Space direction="vertical" className="w-600px mt-[5px]">
-      <div>
-        <Switch size="small" value={value.enabled} onChange={onEnabledChange} />
+    <Space
+      className='w-full'
+      direction='vertical'
+    >
+      <div className='mb-3 font-bold'>{t('page.assistant.labels.document_retrieval')}</div>
+
+      <Form.Item
+        label={t('page.assistant.labels.datasource')}
+        layout='vertical'
+        name={['datasource', 'ids']}
+      >
+        <Select
+          allowClear
+          loading={loading}
+          mode='multiple'
+          options={props.options}
+          value={value.ids}
+        />
+      </Form.Item>
+
+      <div
+        className='mt-10px flex cursor-pointer items-center text-blue-500'
+        onClick={onFilterToggle}
+      >
+        <span>{t('page.assistant.labels.filter')}</span>
+
+        <SvgIcon
+          className='text-20px'
+          icon={showFilter ? 'mdi:chevron-up' : 'mdi:chevron-down'}
+        />
       </div>
-      <Select
-        onChange={onIDsChange}
-        mode="multiple"
-        allowClear
-        options={props.options}
-        value={value.ids}
-        loading={loading}
-      />
-      <div>
-        <Space>
-          <span>{t("page.assistant.labels.show_in_chat")}</span>
-          <Switch
-            value={value.visible}
-            size="small"
-            onChange={onVisibleChange}
+
+      {showFilter && (
+        <Form.Item
+          className='mb-0'
+          name={['datasource', 'filter']}
+        >
+          <Input.TextArea
+            placeholder={filterPlaceHolder}
+            style={{ height: 150 }}
           />
-        </Space>
-      </div>
-      <div>
-        <Space>
-          <span>{t("page.assistant.labels.enabled_by_default")}</span>
+        </Form.Item>
+      )}
+
+      <div className='mt-3 -mb-1'>{t('page.assistant.labels.feature_visibility')}</div>
+
+      <Flex className='[&>div]:(flex-1 m-0!)'>
+        <Form.Item
+          label={t('page.assistant.labels.show_in_chat')}
+          name={['datasource', 'visible']}
+        >
+          <Switch size='small' />
+        </Form.Item>
+
+        <Form.Item
+          label={t('page.assistant.labels.enabled_by_default')}
+          name={['datasource', 'enabled_by_default']}
+        >
           <Switch
-            value={value.enabled_by_default}
-            size="small"
-            onChange={onEnabled_by_defaultChange}
+            disabled={!value.visible}
+            size='small'
           />
-        </Space>
-      </div>
-      <div>
-        <Space direction="vertical">
-          <p
-            className="text-blue-500 mt-10px w-600px flex cursor-pointer items-center"
-            onClick={onFilterToggle}
-          >
-            <span>{t("page.assistant.labels.filter")}</span>{" "}
-            <SvgIcon
-              className="font-size-20px"
-              icon={`${showFilter ? "mdi:chevron-up" : "mdi:chevron-down"}`}
-            />
-          </p>
-          {showFilter && (
-            <Input.TextArea
-              placeholder={filterPlaceHolder}
-              value={value.filter}
-              onChange={onFilterChange}
-              style={{ height: 150 }}
-            />
-          )}
-        </Space>
-      </div>
+        </Form.Item>
+      </Flex>
     </Space>
   );
 };
-
-function isObject(obj: any) {
-  return Object.prototype.toString.call(obj) === "[object Object]";
-}
--- web/src/pages/ai-assistant/modules/UploadConfig.tsx
+++ web/src/pages/ai-assistant/modules/UploadConfig.tsx
@@ -1,20 +1,20 @@
-import { Input, InputNumber, Select, Space, Switch } from "antd";
+import { Input, InputNumber, Select, Space, Switch } from 'antd';
 
 interface UploadConfigProps {
-  value?: any;
-  onChange?: (value: string) => void;
+  readonly value?: any;
+  readonly onChange?: (value: string) => void;
 }
 
 function formatBytes(bytes) {
   if (bytes === 0 || isNaN(bytes) || bytes === null) {
     return {
       value: 1,
-      unit: 1024*1024
+      unit: 1024 * 1024
     };
   }
 
   const base = 1024;
-  const units = [1, 1024, 1024*1024, 1024*1024*1024]
+  const units = [1, 1024, 1024 * 1024, 1024 * 1024 * 1024];
 
   let unitIndex = 0;
   while (bytes >= base && unitIndex < units.length - 1) {
@@ -22,119 +22,134 @@ function formatBytes(bytes) {
     unitIndex++;
   }
 
-  const formattedValue = parseFloat(bytes.toFixed(0));
-  
+  const formattedValue = Number.parseFloat(bytes.toFixed(0));
+
   return {
     value: formattedValue,
     unit: units[unitIndex]
-  }
+  };
 }
 
 export const UploadConfig = (props: UploadConfigProps) => {
   const { t } = useTranslation();
   const { value = {}, onChange } = props;
 
-  const [maxFileSize, setMaxFileSize] = useState({ value: 1, unit: 1024*1024 })
-  const formatRef = useRef(true)
+  const [maxFileSize, setMaxFileSize] = useState({ value: 1, unit: 1024 * 1024 });
+  const formatRef = useRef(true);
 
   useEffect(() => {
     if (formatRef.current && value.max_file_size_in_bytes) {
-      formatRef.current = false
-      setMaxFileSize(formatBytes(value.max_file_size_in_bytes))
+      formatRef.current = false;
+      setMaxFileSize(formatBytes(value.max_file_size_in_bytes));
     }
-  }, [value.max_file_size_in_bytes])
+  }, [value.max_file_size_in_bytes]);
 
   const onEnabledChange = (enabled: boolean) => {
     onChange?.({
       ...value,
       allowed_file_extensions: Array.isArray(value.allowed_file_extensions) ? value.allowed_file_extensions : ['*'],
-      max_file_size_in_bytes: value.max_file_size_in_bytes || 1024*1024,
+      max_file_size_in_bytes: value.max_file_size_in_bytes || 1024 * 1024,
       max_file_count: value.max_file_count || 6,
       enabled
     });
   };
 
   return (
-    <Space direction="vertical" className="w-600px mt-[5px]">
+    <Space
+      className='mt-[5px] w-600px'
+      direction='vertical'
+    >
       <div>
-        <Switch size="small" value={value.enabled} onChange={onEnabledChange} />
+        <Switch
+          size='small'
+          value={value.enabled}
+          onChange={onEnabledChange}
+        />
       </div>
       <div>
-        <Space direction="vertical" className="w-full">
-          <p className="text-[#999] mt-10px">
-            {t("page.assistant.labels.allowed_file_extensions")}
-          </p>
+        <Space
+          className='w-full'
+          direction='vertical'
+        >
+          <p className='mt-10px text-[#999]'>{t('page.assistant.labels.allowed_file_extensions')}</p>
           <Input
+            className='w-full'
             value={Array.isArray(value.allowed_file_extensions) ? value.allowed_file_extensions.join(',') : '*'}
-            onChange={(e) => {
-              const newValue = e.target.value.replace(/[.\s]/g, '')
+            onChange={e => {
+              const newValue = e.target.value.replace(/[.\s]/g, '');
               onChange?.({
-                  ...value,
-                  allowed_file_extensions: newValue ? newValue.split(',') : []
+                ...value,
+                allowed_file_extensions: newValue ? newValue.split(',') : []
               });
             }}
-            className="w-full"
           />
         </Space>
       </div>
       <div>
-        <Space direction="vertical">
-          <p className="text-[#999] mt-10px">
-            {t("page.assistant.labels.max_file_size_in_bytes")}
-          </p>
-          <InputNumber className="w-148px" addonAfter={(
-            <Select value={maxFileSize.unit} className="w-auto" popupMatchSelectWidth={60} onChange={(v) => {
+        <Space direction='vertical'>
+          <p className='mt-10px text-[#999]'>{t('page.assistant.labels.max_file_size_in_bytes')}</p>
+          <InputNumber
+            className='w-148px'
+            min={1}
+            value={maxFileSize.value}
+            addonAfter={
+              <Select
+                className='w-auto'
+                popupMatchSelectWidth={60}
+                value={maxFileSize.unit}
+                onChange={v => {
+                  setMaxFileSize({
+                    ...maxFileSize,
+                    unit: v
+                  });
+                  onChange?.({
+                    ...value,
+                    max_file_size_in_bytes: maxFileSize.value * v
+                  });
+                }}
+              >
+                <Select.Option value={1}>B</Select.Option>
+                <Select.Option value={1024}>KB</Select.Option>
+                <Select.Option value={1024 * 1024}>MB</Select.Option>
+                <Select.Option value={1024 * 1024 * 1024}>GB</Select.Option>
+              </Select>
+            }
+            onChange={v => {
               setMaxFileSize({
                 ...maxFileSize,
-                unit: v
-              })
-              onChange?.({
-                  ...value,
-                  max_file_size_in_bytes: maxFileSize.value * v
+                value: v || 1
               });
-            }}>
-              <Select.Option value={1}>B</Select.Option>
-              <Select.Option value={1024}>KB</Select.Option>
-              <Select.Option value={1024*1024}>MB</Select.Option>
-              <Select.Option value={1024*1024*1024}>GB</Select.Option>
-            </Select>
-          )} min={1} value={maxFileSize.value} onChange={(v) => {
-            setMaxFileSize({
-              ...maxFileSize,
-              value: v || 1
-            })
-            onChange?.({
+              onChange?.({
                 ...value,
                 max_file_size_in_bytes: (v || 1) * maxFileSize.unit
-            });
-          }}/>
-        </Space>
-      </div>
-      <div>
-        <Space direction="vertical">
-          <p className="text-[#999] mt-10px">
-            {t("page.assistant.labels.max_file_count")}
-          </p>
-          <InputNumber
-            min={1}
-            max={100}
-            value={value.max_file_count || 6}
-            onChange={(v) => {
-              onChange?.({
-                  ...value,
-                  max_file_count: v
               });
             }}
           />
         </Space>
       </div>
       <div>
+        <Space direction='vertical'>
+          <p className='mt-10px text-[#999]'>{t('page.assistant.labels.max_file_count')}</p>
+          <InputNumber
+            max={100}
+            min={1}
+            value={value.max_file_count || 6}
+            onChange={v => {
+              onChange?.({
+                ...value,
+                max_file_count: v
+              });
+            }}
+          />
+        </Space>
+      </div>
+      {/* <div>
         <Space direction="vertical">
           <p className="text-[#999] mt-10px">
             {t("page.assistant.labels.caller_model")}
           </p>
         </Space>
-      </div>
+      </div> */}
     </Space>
   );
 };
