42aeee2a0f7fa978e11f9104e49b32e66c3687a3
rain9 <15911122312@163.com>
2025-12-02 09:56:18 +0800

    chore: custom endpoint for widget

--- config/widget/searchbox/wrapper.js
+++ config/widget/searchbox/wrapper.js
@@ -1,5 +1,3 @@
-import { searchbox as originalSearchbox } from "$[[ENDPOINT]]/widgets/searchbox/index.js?v=$[[VER]]";
-
 export async function searchbox(options) {
   if (!options || !options.container) {
     console.error("Container is required in options.");
@@ -14,7 +12,9 @@ export async function searchbox(options) {
 
   const shadow = hostElement.attachShadow({ mode: "open" });
 
-  const linkHref = "$[[ENDPOINT]]/widgets/searchbox/index.css?v=$[[VER]]";
+  const endpoint = options.endpoint || "$[[ENDPOINT]]";
+  const linkHref = endpoint + "/widgets/searchbox/index.css?v=$[[VER]]";
+
   const linkElement = document.createElement("link");
   linkElement.rel = "stylesheet";
   linkElement.href = linkHref;
@@ -26,14 +26,14 @@ export async function searchbox(options) {
     shadow.appendChild(wrapper);
 
     const { searchbox: originalSearchbox } = await import(
-      "$[[ENDPOINT]]/widgets/searchbox/index.js?v=$[[VER]]"
+      endpoint + "/widgets/searchbox/index.js?v=$[[VER]]"
     );
 
     const finalOptions = {
       ...options,
       container: wrapper,
       id: "$[[ID]]",
-      server: "$[[ENDPOINT]]",
+      server: endpoint,
       linkHref
     };
 
