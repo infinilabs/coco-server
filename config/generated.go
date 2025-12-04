56778159cbf3b9fba7cda90e5a6ef414b9fae0df
Steve Lau <stevelauc@outlook.com>
2025-12-04 18:10:45 +0800

    correct mappings: use knn_dense_float_vector rather than dense_vector

--- config/generated.go
+++ config/generated.go
@@ -1,11 +1,11 @@
 package config
 
-const LastCommitLog = "c539a2975852f7a882ba503318a68efcc2d8ec06"
+const LastCommitLog = "N/A"
 
-const BuildDate = "2025-12-04T09:55:00Z"
+const BuildDate = "N/A"
 
-const EOLDate  = "2030-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
