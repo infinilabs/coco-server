945443b9c04fbba87956336c093528b36f117b77
Steve Lau <stevelauc@outlook.com>
2025-12-04 17:07:51 +0800

    update

--- config/generated.go
+++ config/generated.go
@@ -1,11 +1,11 @@
 package config
 
-const LastCommitLog = "b94c51e88725f77b9cc7328d743eed143f8834ca"
+const LastCommitLog = "N/A"
 
-const BuildDate = "2025-12-04T08:42:48Z"
+const BuildDate = "N/A"
 
-const EOLDate  = "2030-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
--- plugins/processors/embedding/processor.go
+++ plugins/processors/embedding/processor.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	log "github.com/cihub/seelog"
-	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/embeddings"
 	"infini.sh/coco/core"
 	"infini.sh/coco/modules/assistant/langchain"
 	"infini.sh/coco/modules/common"
@@ -90,95 +90,98 @@ func (processor *DocumentEmbeddingProcessor) Name() string {
 }
 
 func (processor *DocumentEmbeddingProcessor) Process(ctx *pipeline.Context) error {
-
-	//get message from queue
 	obj := ctx.Get(processor.config.MessageField)
-	if obj != nil {
-		messages := obj.([]queue.Message)
-		if global.Env().IsDebug {
-			log.Tracef("get %v messages from context", len(messages))
-		}
 
-		if len(messages) == 0 {
-			return nil
-		}
+	if obj == nil {
+		log.Warnf("processor [] receives an empty pipeline context", processor.Name())
+		return nil;
+	}
 
-		provider, err := common.GetModelProvider(processor.config.ModelProviderID)
+	messages := obj.([]queue.Message)
+	if global.Env().IsDebug {
+		log.Tracef("get %v messages from context", len(messages))
+	}
+
+	if len(messages) == 0 {
+		return nil
+	}
+
+	provider, err := common.GetModelProvider(processor.config.ModelProviderID)
+	if err != nil {
+		log.Error("failed to get model provider:", err)
+		return err
+	}
+
+	llm := langchain.GetLLM(provider.BaseURL, provider.APIType, processor.config.ModelName, provider.APIKey, "")
+	c := context.Background()
+
+	// Check if the LLM client supports embeddings
+	embedder, ok := llm.(embeddings.EmbedderClient)
+	if !ok {
+		log.Errorf("Model [%s/%s] does not support embeddings", processor.config.ModelProviderID, processor.config.ModelName)
+		return nil
+	}
+
+	for i := range messages {
+		message := &messages[i]
+		pop := message.Data
+		var outputBytes []byte
+
+		doc := core.Document{}
+		err := util.FromJSONBytes(pop, &doc)
 		if err != nil {
-			log.Error("failed to get model provider:", err)
-			return err
+			log.Error("error on handle document:", i, err)
+			continue
 		}
 
-		llm := langchain.GetLLM(provider.BaseURL, provider.APIType, processor.config.ModelName, provider.APIKey, "")
-		c := context.Background()
+		// Skip if text is too short or too long
+		if len(doc.Text) < processor.config.MinInputDocumentLength {
+			log.Debugf("skipping document %s: text length %d < min %d", doc.ID, len(doc.Text), processor.config.MinInputDocumentLength)
+			// Still push to output queue? Maybe yes, but without embedding.
+			// For now, let's push it as is.
+			outputBytes = pop
+		} else {
+			log.Info("start embedding doc: ", doc.ID, ",", doc.Title)
+			start := time.Now()
 
-		// Check if the LLM client supports embeddings
-		embedder, ok := llm.(llms.Embedder)
-		if !ok {
-			log.Errorf("Model provider %s (type: %s) does not support embeddings", processor.config.ModelProviderID, provider.APIType)
-			return nil
-		}
-
-		for i := range messages {
-			message := &messages[i]
-			pop := message.Data
-			var outputBytes []byte
-
-			doc := core.Document{}
-			err := util.FromJSONBytes(pop, &doc)
-			if err != nil {
-				log.Error("error on handle document:", i, err)
-				continue
+			// Truncate if too long (simple truncation, ideally should chunk)
+			textToEmbed := doc.Text
+			if len(textToEmbed) > processor.config.MaxInputDocumentLength {
+				textToEmbed = textToEmbed[:processor.config.MaxInputDocumentLength]
 			}
 
-			// Skip if text is too short or too long
-			if len(doc.Text) < processor.config.MinInputDocumentLength {
-				log.Debugf("skipping document %s: text length %d < min %d", doc.ID, len(doc.Text), processor.config.MinInputDocumentLength)
-				// Still push to output queue? Maybe yes, but without embedding.
-				// For now, let's push it as is.
+			embeddings, err := embedder.CreateEmbedding(c, []string{textToEmbed})
+			if err != nil {
+				log.Errorf("failed to create embedding for doc %s: %v", doc.ID, err)
+				// Push original doc without embedding
 				outputBytes = pop
+			} else if len(embeddings) > 0 {
+				// Convert []float32 to []float64
+				var embedding64 []float64
+				for _, v := range embeddings[0] {
+					embedding64 = append(embedding64, float64(v))
+				}
+				doc.Embedding = embedding64
+				outputBytes = util.MustToJSONBytes(doc)
+				message.Data = outputBytes
+				log.Infof("finished embedding doc, %v, %v, elapsed: %v, dims: %v", doc.ID, doc.Title, util.Since(start), len(doc.Embedding))
 			} else {
-				log.Info("start embedding doc: ", doc.ID, ",", doc.Title)
-				start := time.Now()
-
-				// Truncate if too long (simple truncation, ideally should chunk)
-				textToEmbed := doc.Text
-				if len(textToEmbed) > processor.config.MaxInputDocumentLength {
-					textToEmbed = textToEmbed[:processor.config.MaxInputDocumentLength]
-				}
-
-				embeddings, err := embedder.CreateEmbedding(c, []string{textToEmbed})
-				if err != nil {
-					log.Errorf("failed to create embedding for doc %s: %v", doc.ID, err)
-					// Push original doc without embedding
-					outputBytes = pop
-				} else if len(embeddings) > 0 {
-					// Convert []float32 to []float64
-					var embedding64 []float64
-					for _, v := range embeddings[0] {
-						embedding64 = append(embedding64, float64(v))
-					}
-					doc.Embedding = embedding64
-					outputBytes = util.MustToJSONBytes(doc)
-					message.Data = outputBytes
-					log.Infof("finished embedding doc, %v, %v, elapsed: %v, dims: %v", doc.ID, doc.Title, util.Since(start), len(doc.Embedding))
-				} else {
-					outputBytes = pop
-				}
+				outputBytes = pop
 			}
+		}
 
-			if outputBytes == nil {
-				panic("invalid output")
-			}
+		if outputBytes == nil {
+			panic("invalid output")
+		}
 
-			//push to output queue
-			r := queue.ProduceRequest{Topic: processor.outCfg.ID, Data: outputBytes}
-			res := []queue.ProduceRequest{r}
-			_, err = processor.producer.Produce(&res)
-			if err != nil {
-				panic(errors.Errorf("failed to push message to output queue: %v, %s, offset:%v, size:%v, err:%v", processor.outCfg.Name, processor.outCfg.ID, message.Offset.String(), len(outputBytes), err))
-			}
+		//push to output queue
+		r := queue.ProduceRequest{Topic: processor.outCfg.ID, Data: outputBytes}
+		res := []queue.ProduceRequest{r}
+		_, err = processor.producer.Produce(&res)
+		if err != nil {
+			panic(errors.Errorf("failed to push message to output queue: %v, %s, offset:%v, size:%v, err:%v", processor.outCfg.Name, processor.outCfg.ID, message.Offset.String(), len(outputBytes), err))
 		}
 	}
+
 	return nil
 }
