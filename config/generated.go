83a973b8612f5435c9eb58240ac8361011fdf85f
Steve Lau <stevelauc@outlook.com>
2025-12-04 18:47:49 +0800

    use qianwen and text-embedding-v4 for processor document_embedding

--- config/generated.go
+++ config/generated.go
@@ -1,11 +1,11 @@
 package config
 
-const LastCommitLog = "4d8752a8fe63c176736c333514dbe4d2bc2faa67"
+const LastCommitLog = "N/A"
 
-const BuildDate = "2025-12-04T10:37:50Z"
+const BuildDate = "N/A"
 
-const EOLDate  = "2030-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
