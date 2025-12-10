fa7bfccd746b850d1319ce70ca55e1f906c3027d
Steve Lau <stevelauc@outlook.com>
2025-12-10 15:49:37 +0800

    chore: add license header

--- plugins/processors/extract_file_text/processor.go
+++ plugins/processors/extract_file_text/processor.go
@@ -1,3 +1,7 @@
+/* Copyright © INFINI LTD. All rights reserved.
+ * Web: https://infinilabs.com
+ * Email: hello#infini.ltd */
+
 package extract_file_text
 
 import (
--- plugins/processors/read_file_content/processor.go
+++ plugins/processors/read_file_content/processor.go
@@ -1,3 +1,7 @@
+/* Copyright © INFINI LTD. All rights reserved.
+ * Web: https://infinilabs.com
+ * Email: hello#infini.ltd */
+
 package read_file_content
 
 import (
