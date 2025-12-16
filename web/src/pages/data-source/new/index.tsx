9d5e7fc60259894600fcc40ef852a3ca1498da85
shzhzichen <shzhzichen@gmail.com>
2025-12-16 15:16:23 +0800

    feat: dropbox connector (#614)
    * feat: dropbox connector
    
    * fix: update dropbox screenshot
    
    * fix: update dropbox icons

--- web/src/pages/data-source/new/index.tsx
+++ web/src/pages/data-source/new/index.tsx
@@ -87,6 +87,7 @@ export function Component() {
       case Types.Feishu:
       case Types.Lark:
       case Types.Box:
+      case Types.Dropbox:
         // Feishu/Lark have hardcoded endpoints in backend, only need credentials
         return OAuthValidationPresets.feishuLark;
 
@@ -148,6 +149,8 @@ export function Component() {
         return 'Lark';
       case Types.Box:
         return 'Box';
+      case Types.Dropbox:
+        return 'Dropbox';
       default:
         return connector?.id || type || 'Unknown';
     }
@@ -229,7 +232,8 @@ export function Component() {
       Types.MongoDB,
       Types.Feishu,
       Types.Lark,
-      Types.Box
+      Types.Box,
+      Types.Dropbox
     ].includes(type);
 
   const connectorTypeName = getConnectorTypeName();
@@ -451,6 +455,18 @@ export function Component() {
                   return undefined;
               }
             })()}
+            connectUrl={
+              // eslint-disable-next-line no-nested-ternary
+              type === Types.Feishu
+                ? `/connector/${connector?.id}/feishu/connect`
+                : type === Types.Lark
+                  ? `/connector/${connector?.id}/lark/connect`
+                  : type === Types.Box
+                    ? `/connector/${connector?.id}/box/connect`
+                    : type === Types.Dropbox
+                      ? `/connector/${connector?.id}/dropbox/connect`
+                      : undefined
+            }
           />
         ) : (
           <div>
